81a9e7e336c187864c807e927f0eb250cb2eeb3c
JWHan <96328871+joonwonh@users.noreply.github.com>
2024-02-22 17:24:17 +0900

    feat: spring JDBC Secure 연습

---
+++ 4.4_SpringJDBC_Secure/src/main/java/com/javaEdu/myapp/hr/JDBCEncryptor.java
@@ -0,0 +1,21 @@
+package com.javaEdu.myapp.hr;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+
+public class JDBCEncryptor {
+
+	public static void main(String[] args) {
+		StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
+		enc.setPassword("joonwonh");
+		System.out.println(enc.encrypt("oracle.jdbc.driver.OracleDriver"));
+		System.out.println(enc.encrypt("jdbc:oracle:thin:@localhost:1521:xe"));
+		System.out.println(enc.encrypt("hr"));
+		System.out.println(enc.encrypt("hr"));
+		
+		
+		
+		
+		
+	}
+
+}
